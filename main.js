334947c8fc5e31d123578428c1f16836199afeb4
sherget15 <herget06@gmail.com>
2023-09-21 15:47:23 -0400

    added main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+canvas.width = 600
+canvas.height = 400
+
+let spacePressed = false
+let angle = 0
+let hue = 0
+let frame = 0 
+let score = 0 
+let gamespeed = 2
+
+
