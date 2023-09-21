3cb3a013e9e88847681609cb6a78055c2832206b
sherget15 <herget06@gmail.com>
2023-09-21 16:56:46 -0400

    tweaking anmimate() in main.js

--- main.js
+++ main.js
@@ -13,8 +13,11 @@ let gamespeed = 2
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.fillRect(10, canvas.height - 90, 50, 50)
+    // ctx.fillRect(10, canvas.height - 90, 50, 50)
+    robot.update()
+    robot.draw()
     requestAnimationFrame(animate)
+    angle += 0.12
 }
 animate()
 
